f54e72782ea15fd85e8a0974c4fd907a0833ae73
Igor Ryan <dev@igorryan.com>
2020-08-18 13:56:36 -0300

    envio teste

--- init.js
+++ init.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT || 3000, () => console.log('Executando microsservice
 connectDb().then(async () => {
   console.log('Banco conectado e cron job iniciado.');
   cron.schedule("0 */1 * * *", async () => {
-    console.log('Executando verificação de horário...');
+    console.log('Executando verificação de horário..');
     microsservice();
   });
 });
